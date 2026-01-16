8fba84e0a42facddf2a31befea887bd293661334
woobum <woobum72@gmail.com>
2026-01-16 20:40:06 +0900

    초기 커밋.

---
+++ DXStudy/Game.h
@@ -0,0 +1,21 @@
+#pragma once
+class Game
+{
+public :
+	Game();
+	~Game();
+
+public:
+	void Init(HWND hwnd);
+	void Update();
+	void Render();
+
+private:
+	HWND _hwnd;
+	uint32 _width = 0;
+	uint32 _height = 0;
+
+private:
+	
+};
+
